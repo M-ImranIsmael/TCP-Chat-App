45165287912111d6d35ddb7f38821aa97414d943
M-ImranIsmael <111273105+M-ImranIsmael@users.noreply.github.com>
2022-08-16 12:20:23 +0800

    Add files via upload

---
+++ Server_GUI/Server_GUI/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Server_GUI
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.ServerTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.DisconnectButton.Location = new System.Drawing.Point(22, 12);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(123, 34);
+            this.DisconnectButton.TabIndex = 0;
+            this.DisconnectButton.Text = "Disconnected";
+            this.DisconnectButton.UseVisualStyleBackColor = false;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ExportButton.Location = new System.Drawing.Point(22, 385);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(83, 20);
+            this.ExportButton.TabIndex = 2;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // ServerTextBox
+            // 
+            this.ServerTextBox.Location = new System.Drawing.Point(111, 385);
+            this.ServerTextBox.Name = "ServerTextBox";
+            this.ServerTextBox.Size = new System.Drawing.Size(286, 20);
+            this.ServerTextBox.TabIndex = 3;
+            // 
+            // SendButton
+            // 
+            this.SendButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.SendButton.Location = new System.Drawing.Point(403, 384);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(83, 20);
+            this.SendButton.TabIndex = 4;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(22, 66);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(466, 289);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ServerTextBox);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.DisconnectButton);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.TextBox ServerTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
